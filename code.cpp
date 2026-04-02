130d9ce87cd6f5dc0be7e60389abf32f7be15459
Prem Kathrecha <Premkathrecha07@gmail.com>
2026-04-02 22:35:01 +0530

    Clean up code by removing excessive comments
    Removed extensive commented-out code sections to improve readability.

--- code.cpp
+++ code.cpp
@@ -758,4 +758,19 @@ int main() {
         //     BBB
         //      CC
         //       D 
+
+#include <iostream>
+using namespace std;
+
+int printHello(){
+    cout << "hello\n";
+    return 3;
+}
+
+int main(){
+   int val = printHello();
+   cout << "val =" << val << endl;
+   
+    return 0;
+}
   
